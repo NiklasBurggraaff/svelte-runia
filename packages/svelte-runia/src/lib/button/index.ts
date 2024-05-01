da7ea56b40670ea9121163533b69abbd9667fb1a
Niklas Burggraaff <niklasburggraaff@gmail.com>
2024-05-01 21:45:07 +0100

    Docs general updates

---
+++ packages/svelte-runia/src/lib/button/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from "./Button.svelte";
+export { default as ChildButton } from "./ChildButton.svelte";
+export { default as TextButton } from "./TextButton.svelte";
+export { default as ToggleButton } from "./ToggleButton.svelte";
--- packages/svelte-runia/src/lib/index.ts
+++ packages/svelte-runia/src/lib/index.ts
@@ -1,2 +1,3 @@
 // Reexport your entry components here
 export * as Accordion from "./accordion/index.js";
+export * as Button from "./button/index.js";
